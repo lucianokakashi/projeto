29b1dbbb76fd9144666f4bc9c4c2258c542b5ae4
luciano junior <luciano.junior.r.g@gmail.com>
2024-01-26 14:46:54 -0300

    tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,13 @@
+function togglemode() { 
+  const html= document.documentElement
+
+  if(html.classList.contains('light')){
+    html.classList.remove('light')
+  }else{
+    html.classList.add('light')
+  }
+
+} 
+
+
+
